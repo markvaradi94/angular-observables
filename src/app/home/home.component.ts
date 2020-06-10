5c9db620143fba5dde5ec511791f3b611a23a5ab
Mark Varadi <Mark.Varadi@cloudflight.io>
2020-06-10 17:35:03 +0300

    added observables to project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription, Observable, Observer} from 'rxjs';
+import {map, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private firstObsSubscription: Subscription;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'))
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter(data => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return 'Round ' + data;
+        })
+      )
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        alert(error.message);
+      }, () => {
+        console.log('Observer Completed');
+      })
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-user',
@@ -9,12 +10,17 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private usersService: UserService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    this.usersService.activatedEmitter.next(true);
   }
 }
